cad85b341d2dad1276d824c05045b880a0c97593
amit <amitalvin01@gmail.com>
2023-09-19 01:52:42 +0530

    Modified the name of this project maker

--- ATM INTERFACE USING JAVA/MainClass.java
+++ ATM INTERFACE USING JAVA/MainClass.java
@@ -13,7 +13,7 @@ public class MainClass {
             System.out.println("---------------|| PROJECT : ATM MACHINE ||------------------");
             System.out.println("");
             System.out.println("CREATED BY:");
-            System.out.println("AMIT YADAV\nKASHISH JAIN");
+            System.out.println("AMIT YADAV");
             System.out.println("");
             System.out.println("");
             System.out.println("---------------|| Welcome to ATM Machine ||------------------");
@@ -75,39 +75,38 @@ public class MainClass {
 // These class should be in separate file
 
 // public class ATM {
-//     private double balance;
-//     private double depositAmount;
-//     private double withdrawAmount;
+// private double balance;
+// private double depositAmount;
+// private double withdrawAmount;
 
-//     public ATM() {
+// public ATM() {
 
-//     }
-
-//     public double getBalance() {
-//         return balance;
-//     }
-
-//     public void setBalance(double balance) {
-//         this.balance = balance;
-//     }
-
-//     public double getDepositAmount() {
-//         return depositAmount;
-//     }
-
-//     public void setDepositAmount(double depositAmount) {
-//         this.depositAmount = depositAmount;
-//     }
-
-//     public double getWithdrawAmount() {
-//         return withdrawAmount;
-//     }
-
-//     public void setWithdrawAmount(double withdrawAmount) {
-//         this.withdrawAmount = withdrawAmount;
-//     }
 // }
 
+// public double getBalance() {
+// return balance;
+// }
+
+// public void setBalance(double balance) {
+// this.balance = balance;
+// }
+
+// public double getDepositAmount() {
+// return depositAmount;
+// }
+
+// public void setDepositAmount(double depositAmount) {
+// this.depositAmount = depositAmount;
+// }
+
+// public double getWithdrawAmount() {
+// return withdrawAmount;
+// }
+
+// public void setWithdrawAmount(double withdrawAmount) {
+// this.withdrawAmount = withdrawAmount;
+// }
+// }
 
 // ATM OPERATION IMPLEMENTATION
 // These class should be in separate file
@@ -116,62 +115,59 @@ public class MainClass {
 // import java.util.Map;
 
 // public class AtmOperationImpl implements AtmOperationInterf {
-//     ATM atm = new ATM();
-//     Map<Double, String> ministmt = new HashMap<>();
+// ATM atm = new ATM();
+// Map<Double, String> ministmt = new HashMap<>();
 
-//     @Override
-//     public void viewBalance() {
-//         System.out.println("Available Balance is : " + atm.getBalance());
+// @Override
+// public void viewBalance() {
+// System.out.println("Available Balance is : " + atm.getBalance());
 
-//     }
-
-//     @Override
-//     public void withdrawAmount(double withdrawAmount) {
-//         if (withdrawAmount % 500 == 0) {
-//             if (withdrawAmount <= atm.getBalance()) {
-//                 ministmt.put(withdrawAmount, " Amount Withdrawn");
-//                 System.out.println("Collect the Cash " + withdrawAmount);
-//                 atm.setBalance(atm.getBalance() - withdrawAmount);
-//                 viewBalance();
-//             } else {
-//                 System.out.println("Insufficient Balance !!");
-//             }
-//         } else {
-//             System.out.println("Please enter the amount in multipal of 500");
-//         }
-
-//     }
-
-//     @Override
-//     public void depositAmount(double depositAmount) {
-//         ministmt.put(depositAmount, " Amount Deposited");
-//         System.out.println(depositAmount + " Deposited Successfully !!");
-//         atm.setBalance(atm.getBalance() + depositAmount);
-//         viewBalance();
-
-//     }
-
-//     @Override
-//     public void viewMiniStatement() {
-//         for (Map.Entry<Double, String> m : ministmt.entrySet()) {
-//             System.out.println(m.getKey() + "" + m.getValue());
-//         }
-
-//     }
 // }
 
+// @Override
+// public void withdrawAmount(double withdrawAmount) {
+// if (withdrawAmount % 500 == 0) {
+// if (withdrawAmount <= atm.getBalance()) {
+// ministmt.put(withdrawAmount, " Amount Withdrawn");
+// System.out.println("Collect the Cash " + withdrawAmount);
+// atm.setBalance(atm.getBalance() - withdrawAmount);
+// viewBalance();
+// } else {
+// System.out.println("Insufficient Balance !!");
+// }
+// } else {
+// System.out.println("Please enter the amount in multipal of 500");
+// }
+
+// }
+
+// @Override
+// public void depositAmount(double depositAmount) {
+// ministmt.put(depositAmount, " Amount Deposited");
+// System.out.println(depositAmount + " Deposited Successfully !!");
+// atm.setBalance(atm.getBalance() + depositAmount);
+// viewBalance();
+
+// }
+
+// @Override
+// public void viewMiniStatement() {
+// for (Map.Entry<Double, String> m : ministmt.entrySet()) {
+// System.out.println(m.getKey() + "" + m.getValue());
+// }
+
+// }
+// }
 
 // ATM OPERATION INTERFACE
 // These class should be in separate file
 
 // public interface AtmOperationInterf {
-//     public void viewBalance();
+// public void viewBalance();
 
-//     public void withdrawAmount(double withdrawAmount);
+// public void withdrawAmount(double withdrawAmount);
 
-//     public void depositAmount(double depositAmount);
+// public void depositAmount(double depositAmount);
 
-//     public void viewMiniStatement();
+// public void viewMiniStatement();
 // }
-
-
